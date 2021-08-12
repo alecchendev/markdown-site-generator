428f6f394f27541462a376aaba0ef50195497d34
Alec Chen <alecchen@alecs-air.lan>
2021-08-12 13:35:15 -0500

    Clean up main.go, add print statements

--- main.go
+++ main.go
@@ -17,14 +17,7 @@ var buildDir = "build"
 var staticDir = "static"
 var contentDir = "content"
 var layoutFile = "layout.html"
-
-func createEmptyFile(filename string) {
-	data := []byte("")
-	err := ioutil.WriteFile(filename, data, 0644)
-	if err != nil {
-		panic(err)
-	}
-}
+var fillDiv = "<div id=\"content\"></div>"
 
 func main() {
 
@@ -33,69 +26,76 @@ func main() {
 		srcDir = "."
 	} else {
 		if len(os.Args) > 2 {
-			fmt.Println("Unnecessary extra arguments.")
+			fmt.Println("Unnecessary extra arguments")
 		}
 		srcDir = os.Args[1]
 	}
+	fmt.Printf("Source directory: %v\n", srcDir)
 
 	// Create build directory
 	_, err := os.Stat(buildDir)
 	if os.IsNotExist(err) {
 		err := os.Mkdir(buildDir, 0755)
 		if err != nil {
-			panic(err)
+			log.Fatal(err)
 		}
 	}
+	fmt.Println("Created build directory")
 
 	// Copy src/static to build/static
 	err = copy.Copy(strings.Join([]string{srcDir, staticDir}, "/"), strings.Join([]string{buildDir, staticDir}, "/"))
 	if err != nil {
 		log.Fatal(err)
 	}
+	fmt.Println("Copied static assets")
 
-	// Iterate through .md files in content
+	// Read in layout.html
 	layout, err := ioutil.ReadFile(strings.Join([]string{srcDir, layoutFile}, "/")) // []uint8
 	if err != nil {
 		log.Fatal(err)
 	}
+	layoutStr := string(layout)
 
-	c, err := ioutil.ReadDir(strings.Join([]string{srcDir, contentDir}, "/"))
+	// Check if div content is even there
+	if !strings.Contains(layoutStr, fillDiv) {
+		log.Fatalf("layout.html does not contain %v", fillDiv)
+	}
+
+	// Read in content directory
+	contentFiles, err := ioutil.ReadDir(strings.Join([]string{srcDir, contentDir}, "/"))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	for _, entry := range c {
-		fmt.Println(" ", entry.Name(), entry.IsDir())
+	// Iterate through .md files in content
+	for _, entry := range contentFiles {
 		if entry.IsDir() {
 			continue
 		}
-		// - Read file
+
+		filename := strings.Join([]string{buildDir, strings.Replace(entry.Name(), ".md", ".html", 1)}, "/")
+		fmt.Printf("Creating %v...", filename)
+
+		// Read file
 		fileBytes, err := ioutil.ReadFile(strings.Join([]string{srcDir, contentDir, entry.Name()}, "/")) // []uint8
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Printf("%T\n", fileBytes)
-		fmt.Println(string(fileBytes))
 
-		// - Generate html corresponding to the markdown
+		// Generate html corresponding to the markdown
 		extensions := parser.CommonExtensions | parser.Attributes
 		parser := parser.NewWithExtensions(extensions)
 		html := markdown.ToHTML(fileBytes, parser, nil) // []uint8
-		fmt.Println(string(html))
 
-		// - Create html file from layout.html
-		filename := strings.Join([]string{buildDir, strings.Replace(entry.Name(), ".md", ".html", 1)}, "/")
-		layoutStr := string(layout)
-		// Check if div content is even there
-		output := strings.Replace(layoutStr, "<div id=\"content\"></div>", string(html), 1)
+		// Create html file from layout.html - fill with markdown content
+		output := strings.Replace(layoutStr, fillDiv, string(html), 1)
 		err = ioutil.WriteFile(filename, []byte(output), 0644)
 		if err != nil {
 			log.Fatal(err)
 		}
+		fmt.Print("Done\n")
 
-		// - Fill "content" div with content
 	}
-
-	// createEmptyFile(strings.Join([]string{buildDir, filename}, "/"))
+	fmt.Print("Build complete\n")
 
 }
