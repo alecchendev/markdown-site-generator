1916ff76de1e7a552e1b810f9e01f2f774dc3926
Alec Chen <alecchen@alecs-air.lan>
2021-08-11 19:33:02 -0500

    Implement getting src directory, creating build directory, and copying static assets

--- main.go
+++ main.go
@@ -2,17 +2,56 @@ package main
 
 import (
 	"fmt"
-	// "os"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
 )
+
+var srcDir string
+var buildDir = "build"
+var staticDir = "static"
+var contentDir = "content"
+
+func createEmptyFile(filename string) {
+	data := []byte("")
+	err := ioutil.WriteFile(filename, data, 0644)
+	if err != nil {
+		panic(err)
+	}
+}
 
 func main() {
 
-	// argsWithProg := os.Args
-	// argsWithoutProg := os.Args[1:]
+	// Get src directory
+	if len(os.Args) < 2 {
+		srcDir = "."
+	} else {
+		if len(os.Args) > 2 {
+			fmt.Println("Unnecessary extra arguments.")
+		}
+		srcDir = os.Args[1]
+	}
 
-	// fmt.Println(argsWithProg)
-	// fmt.Println(argsWithoutProg)
+	// Create build directory
+	_, err := os.Stat(buildDir)
+	if os.IsNotExist(err) {
+		err := os.Mkdir(buildDir, 0755)
+		if err != nil {
+			panic(err)
+		}
+	}
 
-	fmt.Println("Hello world!")
+	// Copy src/static to build/static
+	cmd := exec.Command("cp", "-r", strings.Join([]string{srcDir, staticDir}, "/"), strings.Join([]string{buildDir, staticDir}, "/"))
+	err = cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Iterate through .md files in content
+
+	// createEmptyFile(strings.Join([]string{buildDir, filename}, "/"))
 
 }
